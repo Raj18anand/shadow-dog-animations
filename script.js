0d9259785abcfdbe903af6cb9128eb1ef5e7eb9e
Raj Anand <raj1809anand@gmail.com>
2024-01-27 17:08:10 +0530

    added speed

--- script.js
+++ script.js
@@ -1,148 +1,178 @@
-let playerState = 'idle';
+let playerState = "idle";
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = [
+    {
+      name: "Idle",
+      value: "idle",
+    },
+    {
+      name: "Jump",
+      value: "jump",
+    },
+    {
+      name: "Fall",
+      value: "fall",
+    },
+    {
+      name: "Run",
+      value: "run",
+    },
+    {
+      name: "Dizzy",
+      value: "dizzy",
+    },
+    {
+      name: "Sit",
+      value: "sit",
+    },
+    {
+      name: "Roll",
+      value: "roll",
+    },
+    {
+      name: "Bite",
+      value: "bite",
+    },
+    {
+      name: "KO",
+      value: "ko",
+    },
+    {
+      name: "Get Hit",
+      value: "gethit",
+    },
+  ];
 
-    const buttons = [
-        {
-            name: 'Idle',
-            value:'idle'
-        },
-        {
-            name: 'Jump',
-            value: 'jump'
-        },
-        {
-            name: 'Fall',
-            value: 'fall'
-        },
-        {
-            name: 'Run',
-            value: 'run'
-        },
-        {
-            name: 'Dizzy',
-            value: 'dizzy'
-        },
-        {
-            name: 'Sit',
-            value: 'sit'
-        },
-        {
-            name: 'Roll',
-            value: 'roll'
-        },
-        {
-            name: 'Bite',
-            value: 'bite'
-        },
-        {
-            name: 'KO',
-            value: 'ko'
-        },
-        {
-            name: 'Get Hit',
-            value: 'gethit'
-        }
-    ];
-
-
-    buttons.forEach((button, index) => {
-        const buttonElement = document.getElementById(`button${index + 1}`);
-        buttonElement.addEventListener('click', function() {
-            buttonClickHandler(button);
-        });
+  buttons.forEach((button, index) => {
+    const buttonElement = document.getElementById(`button${index + 1}`);
+    buttonElement.addEventListener("click", function () {
+      buttonClickHandler(button);
     });
+  });
 });
 
 function buttonClickHandler(button) {
-    const { name, value } = button;
-    playerState=value;
+  const { name, value } = button;
+  playerState = value;
 }
 
+let staggerFrames = 5;
 
+const range = document.getElementById("range");
 
-const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+range.addEventListener("input", (e) => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+  const rangeWidth = getComputedStyle(e.target).getPropertyValue("width");
+  const labelWidth = getComputedStyle(label).getPropertyValue("width");
+  const numWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
+  const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2);
+  const max = +e.target.max;
+  const min = +e.target.min;
+  const left =
+    value * (numWidth / max) -
+    numLabelWidth / 2 +
+    scale(value, min, max, 10, -10);
+  label.style.left = `${left}px`;
+  label.innerHTML = value;
+  staggerFrames = 10 / value;
+});
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+const CANVAS_WIDTH = (canvas.width = 600);
+const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
+playerImage.src = "shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
 
-
 let gameFrame = 0;
-const staggerFrames = 5;
+
 const spriteAnimations = [];
 const animationStates = [
-    {
-        name: 'idle',
-        frames: 7,
-    },
-    {
-        name: 'jump',
-        frames: 7,
-    },
-    {
-        name: 'fall',
-        frames: 7,
-    },
-    {
-        name: 'run',
-        frames: 9,
-    },
-    {
-        name: 'dizzy',
-        frames: 11,
-    },
-    {
-        name: 'sit',
-        frames: 5,
-    },
-    {
-        name: 'roll',
-        frames: 7,
-    },
-    {
-        name: 'bite',
-        frames: 7,
-    },
-    {
-        name: 'ko',
-        frames: 12,
-    },
-    {
-        name: 'gethit',
-        frames: 4,
-    }
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "gethit",
+    frames: 4,
+  },
 ];
 
-
 animationStates.forEach((state, index) => {
-    let frames = {
-        loc: [],
-    }
-    for (let j = 0; j < state.frames; j++){
-        let positionX = j * spriteWidth;
-        let positionY = index * spriteHeight;
-        frames.loc.push({x: positionX, y: positionY});
-    }
-    spriteAnimations[state.name] = frames;
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
 });
 console.log(spriteAnimations);
 
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spriteAnimations[playerState].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y;
 
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight
+  );
 
-function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
-    let frameX = spriteWidth * position;
-    let frameY = spriteAnimations[playerState].loc[position].y;
-
-    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-
-    gameFrame++;
-    requestAnimationFrame(animate);
-};
+  gameFrame++;
+  requestAnimationFrame(animate);
+}
 animate();
